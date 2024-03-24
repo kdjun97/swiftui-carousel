f6f9c7e8102286a6176946d4ba369969393a75c3
kdjun97 <kdjun97@gmail.com>
2024-03-24 12:32:00 +0900

    init

---
+++ CarouselExample/CarouselExample/CarouselExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselExampleApp.swift
+//  CarouselExample
+//
+//  Created by 김동준 on 3/24/24
+//
+
+import SwiftUI
+
+@main
+struct CarouselExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarouselExample/CarouselExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarouselExample
+//
+//  Created by 김동준 on 3/24/24
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
